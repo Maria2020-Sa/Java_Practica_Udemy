6275a55ae83a0d7676c01ec3124b98add6029dc0
Maria2020-Sa <maugesara@gmail.com>
2023-09-19 13:39:59 -0300

    commit prueba

--- src/main/java/org/example/HolaMundo.java
+++ src/main/java/org/example/HolaMundo.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class HolaMundo {
     //Operadores Ternarios: nos puede ayudar para simplificar una estructura del tipo if / else
+    public static void main(String[] args) {
 
+    }
 
 }
