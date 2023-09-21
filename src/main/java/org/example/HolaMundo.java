8f0ef252808e41554a1cae3064590fd6597d8a2c
Maria2020-Sa <maugesara@gmail.com>
2023-09-21 12:20:20 -0300

    queda en operadores ternarios

--- src/main/java/org/example/HolaMundo.java
+++ src/main/java/org/example/HolaMundo.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class HolaMundo {
     //Operadores Ternarios: nos puede ayudar para simplificar una estructura del tipo if / else
     public static void main(String[] args) {
-
     }
 
 }
