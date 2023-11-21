14795b8cf2b231a14079f0e865939e03e78a3957
Maria2020-Sa <maugesara@gmail.com>
2023-11-21 16:59:28 -0300

    udemy pasando a clases y objetos

--- src/main/java/org/example/OperadorTernario.java
+++ src/main/java/org/example/OperadorTernario.java
@@ -3,7 +3,12 @@ package org.example;
 public class OperadorTernario {
     //Operadores Ternarios: nos puede ayudar para simplificar una estructura del tipo if / else
     public static void main(String[] args) {
-        
+        boolean resultado = (3 > 1) ? true : false;
+        System.out.println("resultado = " + resultado);
+
+        int numero = 7;
+        String parImpar = (numero % 2 == 0) ? "Es par" : "Es impar";
+        System.out.println(numero + " = " + parImpar);
     }
 
 }
