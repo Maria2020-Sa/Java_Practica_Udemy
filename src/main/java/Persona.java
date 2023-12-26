3d4ac894247d427b79f84bf557711d297da1b13b
Maria2020-Sa <maugesara@gmail.com>
2023-12-26 11:17:17 -0300

    actualizado

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,47 +1,32 @@
 public class Persona {
 
-    private String nombre;
-    private double sueldo;
-    private boolean eliminado;
+    private String nombre;//se asocia con un objeto (instancia)
+    private int idPersona;//se asocia con un objeto (instancia)
+    private static int contadorPersonas;
 
-    static String apellido;
-
-    public Persona (String nombre, double sueldo, boolean eliminado){
+    public Persona (String nombre){
         this.nombre = nombre;
-        this.sueldo = sueldo;
-        this.eliminado = eliminado;
+        this.idPersona = ++contadorPersonas;
     }
 
     public String getNombre (){
         return this.nombre;
     }
 
-    public void setNombre (String nombre){
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public double getSueldo (){
-        return this.sueldo;
+    public int getIdPersona (){
+        return this.idPersona;
     }
 
-    public void setSueldo (double sueldo){
-        this.sueldo = sueldo;
-    }
-
-    public boolean isEliminado (){
-        return this.eliminado;
-    }
-
-    public void setEliminado (boolean eliminado){
-        this.eliminado = eliminado;
+    public static int getContadorPersonas () {
+        return contadorPersonas;
     }
 
     @Override
     public String toString() {
-        return "Persona{" +
-                "nombre='" + nombre + '\'' +
-                ", sueldo=" + sueldo +
-                ", eliminado=" + eliminado +
-                '}';
+        return "ID Persona Nª: " + idPersona + ", Nombre: " + nombre + ", Contador Personas: " +contadorPersonas;
     }
 }
---
+++ src/main/java/PracticaEstatico.java
@@ -0,0 +1,10 @@
+public class PracticaEstatico {
+    public static void main(String[] args) {
+        
+        Persona persona1 = new Persona("Maria");
+        System.out.println("persona1 = " + persona1);
+        
+        Persona persona2 = new Persona("Fran");
+        System.out.println("persona2 = " + persona2);
+    }
+}
