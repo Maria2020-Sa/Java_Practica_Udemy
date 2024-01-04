14f446066aea7b2acaa45708eb89e74a412c397b
Maria2020-Sa <maugesara@gmail.com>
2024-01-04 12:30:04 -0300

    practica sobrecarga de metodos

--- src/main/java/SobrecargaMetodos.java
+++ src/main/java/SobrecargaMetodos.java
@@ -3,7 +3,7 @@ public class SobrecargaMetodos {
 
         System.out.println("Resultado 1: " + Operaciones.suma(3, 2));
         System.out.println("Resultado 2: " + Operaciones.suma(6, 2.3));
-        System.out.println("Resultado 3: " + Operaciones.suma(5.6, 8));
+        System.out.println("Resultado 3: " + Operaciones.suma(5F, 'A'));
         System.out.println("Resultado 4: " + Operaciones.suma(9.3, 3.3));
     }
 }
